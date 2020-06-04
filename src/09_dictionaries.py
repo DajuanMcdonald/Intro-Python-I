1916ab0251ec9d4ec2f1095b55f7865ff81c473e
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-06-04 11:48:33 -0400

    not a real place

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,14 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+waypoints.append({"lat": 20, "lon": -90, "name": "middle of nowhere"})
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
 # YOUR CODE HERE
-
+waypoints.insert(0, {"lon": -130, "name": "not a real place"})
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+print(waypoints)
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,5 +1,6 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
+
 # YOUR CODE HERE
 
 # Read a number from the keyboard
