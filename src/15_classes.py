3315480985d9e0f97a89089661f1300322df7677
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-06-14 17:10:11 -0400

    classes

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,19 +1,38 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
+class LatLon:
+	def __init__(self, lat, lon):
+		self.lat = lat
+		self.lon = lon
+
 
 # YOUR CODE HERE
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
-
+class Waypoint(LatLon):
+	def desc(name, lat, lon):
+		name = str(name)
+		lat = float(lat)
+		lon = float(lon)
+		return name, lat, lon
 # YOUR CODE HERE
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache(Waypoint):
+	def desc(name, difficulty, size, lat, lon):
+		name = str(name)
+		difficulty = float(difficulty)
+		size = int(size)
+		lat = float(lat)
+		lon = float(lon)
 
+		return name, difficulty, size, lat, lon
 # YOUR CODE HERE
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
+waypoint = Waypoint.desc("Catacombs", 41.70505, -121.51521)
 
 # YOUR CODE HERE
 
@@ -22,7 +41,7 @@
 print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-
+geocache = Geocache.desc("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 # YOUR CODE HERE
 
 # Print it--also make this print more nicely
